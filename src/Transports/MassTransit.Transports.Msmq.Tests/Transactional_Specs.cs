def328ba6a965eebb52de1228d82eb94d4f9486f
NikGovorov <nikita.govorov@gmail.com>
2011-09-17 14:20:23 +0400

    Failing test.

--- src/Transports/MassTransit.Transports.Msmq.Tests/Transactional_Specs.cs
+++ src/Transports/MassTransit.Transports.Msmq.Tests/Transactional_Specs.cs
@@ -13,6 +13,7 @@
 namespace MassTransit.Transports.Msmq.Tests
 {
     using System;
+    using System.Transactions;
     using Magnum.Extensions;
     using Magnum.TestFramework;
     using MassTransit.Tests;
@@ -109,6 +110,55 @@ namespace MassTransit.Transports.Msmq.Tests
             _future.IsAvailable(3.Seconds()).ShouldBeTrue();
 
             LocalEndpoint.ShouldNotContain(_ping);
+        }
+    }
+
+
+    [TestFixture, Integration]
+    public class When_a_consumer_does_not_complete_nested_transaction_in_the_root_transaction :
+        Given_a_message_is_received_from_a_transactional_queue
+    {
+        private PingMessage _ping;
+        private FutureMessage<Fault<PingMessage, Guid>> _faultFuture;
+
+        protected override void EstablishContext()
+        {
+            base.EstablishContext();
+            _faultFuture = new FutureMessage<Fault<PingMessage, Guid>>();
+
+            LocalBus.SubscribeHandler<PingMessage>(message => {using (new TransactionScope()){}});
+            LocalBus.SubscribeHandler<Fault<PingMessage, Guid>>(message =>
+            {
+                if (_faultFuture.IsAvailable(TimeSpan.Zero))
+                    return;
+
+                _faultFuture.Set(message);
+            });
+
+            LocalBus.ShouldHaveRemoteSubscriptionFor<PingMessage>();
+            LocalBus.ShouldHaveRemoteSubscriptionFor<Fault<PingMessage, Guid>>();
+
+            _ping = new PingMessage();
+
+            LocalBus.Publish(_ping);
+        }
+
+        [Test]
+        public void The_message_should_exist_in_the_error_queue()
+        {
+            LocalErrorEndpoint.ShouldContain(_ping, 5.Seconds());
+        }
+
+        [Test]
+        public void The_message_should_not_exist_in_the_input_queue()
+        {
+            LocalEndpoint.ShouldNotContain(_ping); // it can pass because of infinite processing loop
+        }
+
+        [Test]
+        public void A_fault_should_be_published()
+        {
+            _faultFuture.IsAvailable(3.Seconds()).ShouldBeTrue();
         }
     }
 }
